0b89786530ca9798f539f7f88e1392674f5e474e
RobertDober <robert.dober@gmail.com>
2024-11-25 12:05:31 +0100

    wipping v0.0.0

---
+++ dev/mk_specs.ex
@@ -0,0 +1,15 @@
+defmodule Dev.MkSpecs do
+  @moduledoc ~S"""
+  Generate specs and the according tests from the file test/spec.txt which
+  is defined [here](https://github.com/github/cmark-gfm/blob/master/test/spec.txt)
+  
+  **TODO** Greate a procedure to check if the file is up to date, reload and
+  regenerate the specs and tests if applicable.
+  """
+
+  def run do
+    IO.puts(:stderr, "Coming soon")
+  end
+
+end
+# SPDX-License-Identifier: Apache-2.0
